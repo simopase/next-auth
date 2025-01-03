49d6555a8724d521b04cfe6931aba06fc61825d9
simopase <simopase@hotmail.it>
2025-01-03 22:28:13 +0100

    Fix formatting in sign-out form in login page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -11,7 +11,7 @@ export default async function Login() {
                 action={async () => {
                     "use server"
                     await signOut()
-                }}
+                }} 
             >
                 <button type="submit">Sign Out</button>
             </form>
