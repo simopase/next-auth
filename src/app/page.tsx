3888b591be9cc6e9106f9f4ead4e76c0510eb20d
simopase <simopase@hotmail.it>
2025-01-03 22:09:14 +0100

    Add Login component for GitHub authentication

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import Login from "@/components/Login/Login";
 
 export default function Home() {
   return (
@@ -46,6 +47,7 @@ export default function Home() {
           </a>
         </div>
       </main>
+      <Login></Login>
       <footer className={styles.footer}>
         <a
           href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
